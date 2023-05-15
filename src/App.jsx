aaf812365f71efe4b5b8887828df15ade8b3be35
Wisnu Box <hwisnu222@gmail.com>
2023-05-15 10:50:26 +0700

    feat: useContext subject

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,29 @@
-import React from "react";
+import React, { useContext } from "react";
+
+const CountContext = React.createContext();
+
+const CountComponent = () => {
+  const [state, dispatch] = useContext(CountContext);
+
+  const increment = () => {
+    dispatch((prev) => prev + 1);
+  };
+  return (
+    <>
+      <p>{state}</p>
+      <button onClick={increment}>Increament</button>
+    </>
+  );
+};
 
 function App() {
-  return <div></div>;
+  const [state, setState] = React.useState(0);
+
+  return (
+    <CountContext.Provider value={[state, setState]}>
+      <CountComponent />
+    </CountContext.Provider>
+  );
 }
 
 export default App;
